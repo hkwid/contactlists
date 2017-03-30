02bc6c711b2c184ec01fa6083c4a77ca289173e5
Hironori Kawaida <hk@nowdiscover.com>
2017-03-30 08:29:32 +0200

    Modify test code for objectArraySort

--- tests/resources/sortedObjects.js
+++ tests/resources/sortedObjects.js
@@ -14,10 +14,26 @@ export const originalObject = [
     "email": "jordan.flowers@hopeli.us",
     "phone": "+1 (879) 584-3181",
     "address": "998 Schenectady Avenue, Tyhee, New York, 4990"
+  },
+  {
+    "firstName": "Eve",
+    "lastName": "Howard",
+    "company": "IMANT",
+    "email": "eve.howard@imant.tv",
+    "phone": "+1 (896) 500-2985",
+    "address": "970 Brighton Avenue, Cedarville, West Virginia, 5553"
   }
 ];
 
 export const sortedByFirstNameObject = [
+  {
+    "firstName": "Eve",
+    "lastName": "Howard",
+    "company": "IMANT",
+    "email": "eve.howard@imant.tv",
+    "phone": "+1 (896) 500-2985",
+    "address": "970 Brighton Avenue, Cedarville, West Virginia, 5553"
+  },
   {
     "firstName": "Jordan",
     "lastName": "Flowers",
@@ -46,6 +62,14 @@ export const sortedByLastNameObject = [
     "address": "998 Schenectady Avenue, Tyhee, New York, 4990"
   },
   {
+    "firstName": "Eve",
+    "lastName": "Howard",
+    "company": "IMANT",
+    "email": "eve.howard@imant.tv",
+    "phone": "+1 (896) 500-2985",
+    "address": "970 Brighton Avenue, Cedarville, West Virginia, 5553"
+  },
+  {
     "firstName": "Mccullough",
     "lastName": "Stephenson",
     "company": "ZAJ",
@@ -56,6 +80,68 @@ export const sortedByLastNameObject = [
 ];
 
 export const sortedByPhoneObject = [
+  {
+    "firstName": "Jordan",
+    "lastName": "Flowers",
+    "company": "HOPELI",
+    "email": "jordan.flowers@hopeli.us",
+    "phone": "+1 (879) 584-3181",
+    "address": "998 Schenectady Avenue, Tyhee, New York, 4990"
+  },
+  {
+    "firstName": "Eve",
+    "lastName": "Howard",
+    "company": "IMANT",
+    "email": "eve.howard@imant.tv",
+    "phone": "+1 (896) 500-2985",
+    "address": "970 Brighton Avenue, Cedarville, West Virginia, 5553"
+  },
+  {
+    "firstName": "Mccullough",
+    "lastName": "Stephenson",
+    "company": "ZAJ",
+    "email": "mccullough.stephenson@zaj.com",
+    "phone": "+1 (977) 406-2079",
+    "address": "895 Landis Court, Hessville, Delaware, 9608"
+  }
+];
+
+export const sortedByAddressObject = [
+  {
+    "firstName": "Mccullough",
+    "lastName": "Stephenson",
+    "company": "ZAJ",
+    "email": "mccullough.stephenson@zaj.com",
+    "phone": "+1 (977) 406-2079",
+    "address": "895 Landis Court, Hessville, Delaware, 9608"
+  },
+  {
+    "firstName": "Eve",
+    "lastName": "Howard",
+    "company": "IMANT",
+    "email": "eve.howard@imant.tv",
+    "phone": "+1 (896) 500-2985",
+    "address": "970 Brighton Avenue, Cedarville, West Virginia, 5553"
+  },
+  {
+    "firstName": "Jordan",
+    "lastName": "Flowers",
+    "company": "HOPELI",
+    "email": "jordan.flowers@hopeli.us",
+    "phone": "+1 (879) 584-3181",
+    "address": "998 Schenectady Avenue, Tyhee, New York, 4990"
+  }
+];
+
+export const sortedByEmailObject = [
+  {
+    "firstName": "Eve",
+    "lastName": "Howard",
+    "company": "IMANT",
+    "email": "eve.howard@imant.tv",
+    "phone": "+1 (896) 500-2985",
+    "address": "970 Brighton Avenue, Cedarville, West Virginia, 5553"
+  },
   {
     "firstName": "Jordan",
     "lastName": "Flowers",
--- tests/utils/objectArraySort.test.js
+++ tests/utils/objectArraySort.test.js
@@ -3,7 +3,9 @@ import {
   originalObject,
   sortedByFirstNameObject,
   sortedByLastNameObject,
-  sortedByPhoneObject
+  sortedByPhoneObject,
+  sortedByAddressObject,
+  sortedByEmailObject
 } from '../resources/sortedObjects';
 
 describe('utils/objectArraySort', () => {
@@ -13,12 +15,22 @@ describe('utils/objectArraySort', () => {
   });
 
   it('should return sorted by lastName Object', () => {
-    const sortedObject = getSortedObjectArray(originalObject, 'firstName', 'asc');
+    const sortedObject = getSortedObjectArray(originalObject, 'lastName', 'asc');
     expect(sortedObject).toEqual(sortedByLastNameObject);
   });
 
-  it('should return sorted by Phone Object', () => {
-    const sortedObject = getSortedObjectArray(originalObject, 'firstName', 'asc');
+  it('should return sorted by phone Object', () => {
+    const sortedObject = getSortedObjectArray(originalObject, 'phone', 'asc');
     expect(sortedObject).toEqual(sortedByPhoneObject);
+  });
+
+  it('should return sorted by address Object', () => {
+    const sortedObject = getSortedObjectArray(originalObject, 'address', 'asc');
+    expect(sortedObject).toEqual(sortedByAddressObject);
+  });
+
+  it('should return sorted by email Object', () => {
+    const sortedObject = getSortedObjectArray(originalObject, 'email', 'asc');
+    expect(sortedObject).toEqual(sortedByEmailObject);
   });
 });
